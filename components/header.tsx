89782ea0519f4abbf3fa5f4e3982f3c8772478f9
Rahul Jaiswal <rjrahool007@gmail.com>
2025-12-29 14:35:40 +0530

    Add logos to navbar

--- components/header.tsx
+++ components/header.tsx
@@ -13,6 +13,7 @@ import { CartButton } from "@/components/cart-button";
 import { NotificationProvider } from "@/components/notification-provider";
 import { SignOutButton } from "@/components/sign-out-button";
 import { IoFastFood, IoFastFoodOutline } from "react-icons/io5";
+import Image from "next/image";
 export async function Header() {
   const supabase = await createClient();
   const {
@@ -32,14 +33,22 @@ export async function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
-        <Link href="/" className="flex items-center space-x-2">
-          <IoFastFoodOutline className="h-8 w-8 text-primary" />
-          <span className="text-xl font-bold text-balance hidden sm:inline">
+        <Link href="/" className="flex items-center gap-0">
+          <Image
+            src={"/images/logo/Bee_logo_black_transparent.png"}
+            alt="BeeBites Logo"
+            width={150}
+            height={150}
+            className="-mr-10 -mt-4"
+          />
+          {/* <IoFastFoodOutline className="h-8 w-8 text-primary" /> */}
+          {/* <span className="text-xl font-bold text-balance hidden sm:inline">
             BeeBites
-          </span>
-          <span className="text-xl font-bold text-balance sm:hidden">
+          </span> */}
+          {/* <span className="text-xl font-bold text-balance sm:hidden">
             BeeBites
-          </span>
+          </span> */}
+          <span className="text-xl font-bold">Bites</span>
         </Link>
 
         <nav className="flex items-center space-x-2 md:space-x-4">
